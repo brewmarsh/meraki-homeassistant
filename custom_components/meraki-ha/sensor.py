68191a956be09869148fc326c2bc5deb98fcea5a
80M <admarsha@gmail.com>
2025-03-11 09:40:11 -0500

    Added Meraki MX uplink sensors

--- custom_components/meraki-ha/sensor.py
+++ custom_components/meraki-ha/sensor.py
@@ -19,31 +19,58 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the Meraki sensor platform."""
-    _LOGGER.debug("Meraki HA: async_setup_entry in sensor.py called")
     coordinator = hass.data[DOMAIN][config_entry.entry_id][DATA_COORDINATOR]
 
     await coordinator.async_config_entry_first_refresh()
 
-    sensors = [MerakiDeviceSensor(coordinator, device) for device in coordinator.data]
+    sensors = []
+    for device in coordinator.data:
+        sensors.append(MerakiDeviceSensor(coordinator, device))
+        if device.get("uplinks"):
+            for interface, uplink_data in device["uplinks"].items():
+                sensors.append(MerakiUplinkSensor(coordinator, device, interface, uplink_data))
 
     async_add_entities(sensors)
 
 class MerakiDeviceSensor(CoordinatorEntity, SensorEntity):
     """Representation of a Meraki Device Sensor."""
+    # ... (Your existing MerakiDeviceSensor code)
 
-    def __init__(self, coordinator, device):
-        """Initialize the sensor."""
+class MerakiUplinkSensor(CoordinatorEntity, SensorEntity):
+    """Representation of a Meraki Uplink Sensor."""
+
+    def __init__(self, coordinator, device, interface, uplink_data):
+        """Initialize the uplink sensor."""
         super().__init__(coordinator)
         self._device = device
-        self._attr_name = device["name"]
-        self._attr_unique_id = device["serial"]
-        self._attr_state = device.get("firmware", "Unknown")
-        self._attr_extra_state_attributes = {
-            "model": device["model"],
-            "mac": device["mac"],
-            "publicIp": device.get("publicIp"),
+        self._interface = interface
+        self._uplink_data = uplink_data
+        self._attr_name = f"{device['name']} {interface} Uplink"
+        self._attr_unique_id = f"{device['serial']}-{interface}-uplink"
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        if self._uplink_data.get("enabled"):
+            return "Enabled"
+        else:
+            return "Disabled"
+
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes."""
+        attributes = {
+            "vlan_enabled": self._uplink_data.get("vlanTagging", {}).get("enabled"),
+            "vlan_id": self._uplink_data.get("vlanTagging", {}).get("vlanId"),
+            "ipv4_assignment": self._uplink_data.get("svis", {}).get("ipv4", {}).get("assignmentMode"),
+            "ipv4_address": self._uplink_data.get("svis", {}).get("ipv4", {}).get("address"),
+            "ipv4_gateway": self._uplink_data.get("svis", {}).get("ipv4", {}).get("gateway"),
+            "ipv4_nameservers": self._uplink_data.get("svis", {}).get("ipv4", {}).get("nameservers", {}).get("addresses"),
+            "pppoe_enabled": self._uplink_data.get("pppoe", {}).get("enabled"),
+            "pppoe_username": self._uplink_data.get("pppoe", {}).get("authentication", {}).get("username"),
             ATTR_ATTRIBUTION: ATTRIBUTION,
         }
+        return {k: v for k, v in attributes.items() if v is not None}
 
     @property
     def device_info(self):
@@ -55,8 +82,3 @@ class MerakiDeviceSensor(CoordinatorEntity, SensorEntity):
             "model": self._device["model"],
             "sw_version": self._device.get("firmware"),
         }
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._attr_state
