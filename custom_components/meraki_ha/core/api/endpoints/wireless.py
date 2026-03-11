6556686ad7d09090f47bc632664b9c48c4bd6039
80M <admarsha@gmail.com>
2026-03-11 17:11:04 -0500

    Pass groupPolicyId as None to satisfy Meraki Python library positional argument requirement

--- custom_components/meraki_ha/core/api/endpoints/wireless.py
+++ custom_components/meraki_ha/core/api/endpoints/wireless.py
@@ -322,10 +322,8 @@ class WirelessEndpoints:
         # Prepare kwargs for the library call
         kwargs: dict[str, Any] = {
             "name": name,
+            "groupPolicyId": group_policy_id,
         }
-
-        if group_policy_id is not None:
-            kwargs["groupPolicyId"] = group_policy_id
 
         if passphrase:
             kwargs["passphrase"] = passphrase
